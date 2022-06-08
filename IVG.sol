f6e8817b273308b1b04d8dc1fddaf0ec99499371
molu0219 <88714881+molu0219@users.noreply.github.com>
2022-06-08 16:11:53 +0800

    add comments
    add comments

--- IVG.sol
+++ IVG.sol
@@ -18,17 +18,23 @@ contract invisiblegoblin is ERC721Enumerable, Ownable {
     uint256 public maxMintAmount = 10; 
     uint256 public currentIndex = 0;
 
+    // will be executed once when contract deployed
     constructor() ERC721("Invisible Goblin v6", "IVG") payable {
         safeMint(msg.sender, 0);
     }
 
-
+    // mint function only for owner
     function safeMint(address to, uint256 tokenId) private onlyOwner {
         _safeMint(to, tokenId);
+        
+        // count the cuurent token ID
         currentIndex += 1;
 
     }
+    
+    // ERC721 mint function with criterion check
     function mint(uint256 _mintAmount) public payable {
+       
         require(paused != true, "Sale must be active");
         require(_mintAmount > 0); 
         require(_mintAmount +balanceOf(msg.sender) <= maxMintAmount, "You can only adopt 10 Invisible Goblin");
@@ -46,14 +52,17 @@ contract invisiblegoblin is ERC721Enumerable, Ownable {
         }
     }
 
+    // check IPFS json file URL
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
     }
-
+    
+    // set IPFS json file URL ("ipfs://xxxxxx")
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
-
+    
+    // return the token ID own by the owner
     function walletOfOwner(address _owner) public view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
@@ -62,7 +71,8 @@ contract invisiblegoblin is ERC721Enumerable, Ownable {
         }
         return tokenIds;
     }
-
+    
+    // return ipfs info of by token ID
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(
             _exists(tokenId),
@@ -72,24 +82,28 @@ contract invisiblegoblin is ERC721Enumerable, Ownable {
         string memory currentBaseURI = baseURI;
         return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, "/", Strings.toString(tokenId), ".json")) : "";
     }
-
+    
+    // return the minted token number 
     function totalSupply() public view override returns (uint256) {
         return currentIndex;
     }
-
+    
+    // set mint price
     function setCost(uint256 _newCost) public onlyOwner() {
         cost = _newCost;
     }
     
-
+    // set max supply
     function setMaxSupply(uint256 _newMaxSupply) public onlyOwner() {
         maxSupply = _newMaxSupply;
     }
     
+    // set maxs mint ammount
     function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
         maxMintAmount = _newmaxMintAmount;
     }
     
+    // set sales start
     function pause() public onlyOwner {
         paused = !paused;
     }
